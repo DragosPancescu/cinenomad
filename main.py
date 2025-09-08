35d60a9b78c44df3c6d388e4f242d8837af17d4c
dragos <mineralwater19@gmail.com>
2025-09-08 10:38:55 +0300

    Added required folders creation and checks at program start

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import platform
 from components import App
 from utils import set_proc_name
 from utils.database import schema
+from utils.file_handling import load_yaml_file
 
 
 def main():
@@ -14,9 +15,18 @@ def main():
 
         # Ensure Xlib is thread-safe
         ctypes.CDLL("libX11.so").XInitThreads()
+    
+    # Create required folders if first run
+    required_folders = load_yaml_file(os.path.join("settings", "required_folders.yaml"))
+    for folder_path in required_folders:
+        if isinstance(folder_path, list):
+           folder_path = os.path.join(*folder_path)
+        
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 
     config_path = os.path.join("settings", "components_config.yaml")
-
+    
     # Ensure sqlite3 database is created along with the schema
     schema.create_tables()
 
@@ -27,4 +37,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
